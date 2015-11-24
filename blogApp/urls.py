7901295f1de8c03139843bdf0f538f34b1b065dd
Nikhil Sardana <nikhilklk@gmail.com>
2015-11-25 00:09:52 +0530

    Some more changes, extended the application

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -5,5 +5,7 @@ urlpatterns=[
     url(r'^$',views.post_list,name='post_list'),
     #post_list is a view (function) inside views.py that we will create
     #name=post_list is the name of url that we will use later on
+
+    url(r'^post/(?P<pk>[0-9]+)/$',views.post_detail, name='post_detail'),
     
     ]
--- blogApp/views.py
+++ blogApp/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render,get_object_or_404
 
 def post_list(request): #post_list view created
     posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'blogApp/post_list.html',{'posts':posts})
 #display post_list.html
+
+def post_detail(request,pk):
+    #pk is the primary key of the post and is passed on by the urls.py file to this view
+
+
+    #post = Post.objects.get(pk=pk) can be used, but abad looking error is returned when the requested page is not found
+
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blogApp/post_detail.html',{'post':post})
